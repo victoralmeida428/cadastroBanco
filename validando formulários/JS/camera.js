c05453b9c75bc8fb0bdcac6a16fe50cec96c250f
Victor Gomes <117787474+victoralmeida428@users.noreply.github.com>
2023-01-03 20:26:43 -0300

    Update camera.js

--- validando formulários/JS/camera.js
+++ validando formulários/JS/camera.js
@@ -1,40 +1,39 @@
-const btnIniciarCamera = document.querySelector('[data-video-botao]');
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
-const video = document.querySelector('[data-video]');
-const bntTirarFoto = document.querySelector('[data-tirar-foto]');
-const canvas = document.querySelector('[data-video-canvas]');
-const mensagem = document.querySelector('[data-mensagem]');
-const btnEnviarFoto = document.querySelector('[data-enviar]')
+const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]")
 
-let imagemURL = ""
+let imagemURL = '';
 
-btnIniciarCamera.addEventListener('click', async function () {
+botaoIniciarCamera.addEventListener('click', async function () {
     const iniciarVideo = await navigator.mediaDevices
         .getUserMedia({ video: true, audio: false });
 
-    btnIniciarCamera.style.display = "none";
+    botaoIniciarCamera.style.display = "none";
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
 });
 
-bntTirarFoto.addEventListener("click", function() {
-    canvas.getContext("2d").drawImage(video, 20, 20, canvas.width, canvas.height);
+botaoTirarFoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    imagemURL = canvas.toDataURL("image/jpeg");
+    imagemURL = canvas.toDataURL('image/jpeg');
 
     campoCamera.style.display = "none";
+    mensagem.style.display = "block";
+});
 
-    mensagem.style.display = "block"
-})
-
-btnEnviarFoto.addEventListener('click', function(){
+botaoEnviarFoto.addEventListener('click', () => {
     const receberDadosExistentes = localStorage.getItem("cadastro");
     const converteRetorno = JSON.parse(receberDadosExistentes);
+
     converteRetorno.imagem = imagemURL;
 
-    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno))
 
-    window.location.href = "./pages/abrir-conta-form-3.html"
+    window.location.href = '../pages/abrir-conta-form-3.html';
 })
-
