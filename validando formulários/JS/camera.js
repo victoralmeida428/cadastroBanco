9b05b7a905d8d949e23febdd596ec284ea88ddfc
Victor Gomes <117787474+victoralmeida428@users.noreply.github.com>
2023-01-03 19:10:08 -0300

    Update camera.js

--- validando formulários/JS/camera.js
+++ validando formulários/JS/camera.js
@@ -1,13 +1,27 @@
 const btnIniciarCamera = document.querySelector('[data-video-botao]');
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector('[data-video]');
+const bntTirarFoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
 
-    btnIniciarCamera.addEventListener('click', async function () {
-        const iniciarVideo = await navigator.mediaDevices
-            .getUserMedia({ video: true, audio: false });
 
-        btnIniciarCamera.style.display = "none";
-        campoCamera.style.display = "block";
+btnIniciarCamera.addEventListener('click', async function () {
+    const iniciarVideo = await navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false });
 
-        video.srcObject = iniciarVideo;
-    });
+    btnIniciarCamera.style.display = "none";
+    campoCamera.style.display = "block";
+
+    video.srcObject = iniciarVideo;
+});
+
+bntTirarFoto.addEventListener("click", function() {
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+
+    mensagem.style.display = "block"
+})
